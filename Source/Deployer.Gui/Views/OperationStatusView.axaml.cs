f03415e61b4767b4cf38becc0e855f20a01ba0cf
José Manuel Nieto <superjmn@outlook.com>
2023-07-02 16:26:52 +0200

    Latest Avalonia 0.10

--- Source/Deployer.Gui/Views/OperationStatusView.axaml.cs
+++ Source/Deployer.Gui/Views/OperationStatusView.axaml.cs
@@ -1,18 +1,12 @@
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace Deployer.Gui.Views
 {
-    public class OperationStatusView : UserControl
+    public partial class OperationStatusView : UserControl
     {
         public OperationStatusView()
         {
             InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
         }
     }
 }
--- Source/Deployer.Gui/Views/Requirements/DiskRequirementView.axaml.cs
+++ Source/Deployer.Gui/Views/Requirements/DiskRequirementView.axaml.cs
@@ -1,18 +1,12 @@
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace Deployer.Gui.Views.Requirements
 {
-    public class DiskRequirementView : UserControl
+    public partial class DiskRequirementView : UserControl
     {
         public DiskRequirementView()
         {
             InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
         }
     }
 }
